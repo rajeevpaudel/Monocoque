9e9bf3010a5ddbddc886400dd1ea30e291ba29ae
rajeevpaudel <rajeevpaudel58@gmail.com>
2026-05-26 12:46:38 +0545

    feat(dbt): add segment arrays to stg_openf1__laps

---
+++ dbt/models/staging/stg_openf1__laps.sql
@@ -0,0 +1,19 @@
+SELECT
+    session_key,
+    driver_number,
+    lap_number,
+    toDateTime(date_start)          AS lap_start,
+    lap_duration,
+    is_pit_out_lap,
+    duration_sector_1,
+    duration_sector_2,
+    duration_sector_3,
+    i1_speed,
+    i2_speed,
+    st_speed,
+    segments_sector_1,
+    segments_sector_2,
+    segments_sector_3,
+    _ingested_at,
+    'openf1'                        AS _source
+FROM {{ source('raw_openf1', 'laps') }}
